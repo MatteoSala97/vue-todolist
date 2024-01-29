8f03dbaac5dedf209fc2b97c069f89cded599991
MatteoSala97 <matteo111997@gmail.com>
2024-01-29 13:50:00 +0100

    feat: html body and js array of obj

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,31 @@ const {
 
 createApp({
     data(){
-        return{
-            message: "Hello Vue",
+        return {
+            message: "To Boolist",
+            newTask: "",
+            toDos: [
+                { 
+                    text: "Complete work assignment", 
+                    done: false 
+                },
+                { 
+                    text: "Buy groceries for the week", 
+                    done: false 
+                },
+                { 
+                    text: "Go for a 20-minute walk", 
+                    done: false 
+                },
+                { 
+                    text: "Call a friend or family member", 
+                    done: false 
+                },
+                { 
+                    text: "Organize desk and workspace", 
+                    done: false 
+                }
+            ]
         }
     },
     created(){
